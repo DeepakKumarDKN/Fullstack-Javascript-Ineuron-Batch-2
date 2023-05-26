6f713f3c8661518c555e74847dadff1b6223d495
Deepak <deepaknnayak34@gmail.com>
2023-05-26 11:45:06 +0530

    updated

--- Javascript/5th Feb A quick Walkthrough With Javascript Part 2/file.js
+++ Javascript/5th Feb A quick Walkthrough With Javascript Part 2/file.js
@@ -1,24 +1,8 @@
-function sum(){
-    let a = 10 
-    let b = 20 
-    sum = a+b
-    return sum
+function displayName(){
+    let username = "deepak"
+    function dispplayDetails(){
+        console.log(username)
+    }
+    dispplayDetails()
 }
-console.log(sum())
-
-function sub(){
-
-    let a =20
-    let b = 30 
-    sub = (a-b)
-    console.log(sub)
-}
-sub()
-
-function product(){
-    let num1 = 10 
-    let num2 = 20
-    pro = num1*num2
-    console.log(pro)
-}
-product()
+displayName()
