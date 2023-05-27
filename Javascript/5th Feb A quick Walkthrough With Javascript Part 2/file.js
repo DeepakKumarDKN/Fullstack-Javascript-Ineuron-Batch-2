8582242b1a824c0971bf0b13b03c84e28f323c8b
Deepak <deepaknnayak34@gmail.com>
2023-05-27 18:27:20 +0530

    updated

--- Javascript/5th Feb A quick Walkthrough With Javascript Part 2/file.js
+++ Javascript/5th Feb A quick Walkthrough With Javascript Part 2/file.js
@@ -1,14 +1,44 @@
-function Outer(){
-    let name = "deepak"
-    function Inner(){
-        let surname = "nayak"
-        function deep(){
-            console.log(name)
-            console.log(surname)
-        }
-        deep()
-    }
-    Inner()
+// function Outer(){
+//     let name = "deepak"
+//     function Inner(){
+//         let surname = "nayak"
+//         function deep(){
+//             console.log(name)
+//             console.log(surname)
+//         }
+//         deep()
+//     }
+//     Inner()
 
+// }
+// Outer()
+
+// function profile() {
+//     const fullName = "Tobi Sho";
+//     function sayName() {
+//       const fullName = "Oluwa Sofe";
+//       function writeName() {
+//         return fullName;
+//       }
+//       return writeName();
+//     }
+//     return sayName();
+//   }
+// console.log(profile())
+
+
+let userName = "deepak"
+
+function displayData(){
+    let gmail = "deepak@gmail.com";
+    let courseTaken = 1;
+    function courseName(){
+        let courseName = 'fullstack javascript 2.0'
+        function displayCourseName(){
+            console.log(`${courseName} ${userName}`)
+        }
+        displayCourseName()
+    }
+    courseName()
 }
-Outer()
+displayData()
