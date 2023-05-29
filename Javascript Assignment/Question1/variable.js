c22acd221215aec3ed76f3d488da3676a02be465
Deepak <deepaknnayak34@gmail.com>
2023-05-29 09:45:05 +0530

    assignment one solution added

---
+++ Javascript Assignment/Question1/variable.js
@@ -0,0 +1,14 @@
+let username = "deepak";
+let Login = true;
+
+let Location;
+let number = null;
+
+console.log(`
+
+value is: ${username} and datatype is: ${typeof username}
+value is: ${Login} and datatype is: ${typeof Login}
+value is: ${Location} and datatype is: ${typeof Location}
+value is: ${number} and datatype is: ${typeof number}
+
+`)
