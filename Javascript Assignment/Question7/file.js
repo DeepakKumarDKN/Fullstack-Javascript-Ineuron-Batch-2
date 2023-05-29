ab0a020f0c7cd587e95ca10464dc1a788698b3ee
Deepak <deepaknnayak34@gmail.com>
2023-05-29 17:06:24 +0530

    solution 7 added

--- Javascript Assignment/Question7/file.js
+++ Javascript Assignment/Question7/file.js
@@ -1,4 +1,10 @@
 // 07. Declare an array containing the multiple values and use lastIndexOf 
 // to determine the position of the first and last occurrence of a word "pw skills".
 
+//last index always start to search from backward
 
+
+let arrayOne = ['html','css','pw skills','react','javascript','pw skills']
+
+console.log(`The first Occurence of the word 'pw skills' is ${arrayOne.lastIndexOf("pw skills",4)}`)
+console.log(`The last Occurence of the word 'pw skills' is ${arrayOne.lastIndexOf("pw skills")}`)
