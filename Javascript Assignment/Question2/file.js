683aadec6e90f6d11f02ce274aeacd27f539d21f
Deepak <deepaknnayak34@gmail.com>
2023-05-29 09:51:15 +0530

    question 2 solutuon added

---
+++ Javascript Assignment/Question2/file.js
@@ -0,0 +1,13 @@
+let firstname = "deepak kumar";
+let lastname = "nayak";
+let maritialStatus = 'unmarried';
+let country = "India";
+let age = 21;
+
+console.log(`
+    ${firstname}
+    ${lastname}
+    ${maritialStatus}
+    ${country}
+    ${age}
+`)
