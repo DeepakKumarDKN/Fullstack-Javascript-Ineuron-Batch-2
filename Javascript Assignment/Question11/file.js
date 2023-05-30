e7fe9130d75ca5ec296a850e6deaa03034b012c1
Deepak <deepaknnayak34@gmail.com>
2023-05-31 00:11:08 +0530

    question 11 solution added

--- Javascript Assignment/Question11/file.js
+++ Javascript Assignment/Question11/file.js
@@ -9,8 +9,9 @@
 
 const date = new Date()
 console.log(date.getFullYear())
-console.log(date.getMonth())
 console.log(date.getDate())
 console.log(date.getDay())
 console.log(date.getHours())
 console.log(date.getMinutes())
+console.log(date.getMonth() + 1)
+
