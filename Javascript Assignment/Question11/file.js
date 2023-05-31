f756434036a263042c885b5083940cea5fa712d1
Deepak <deepaknnayak34@gmail.com>
2023-05-31 08:25:06 +0530

    question 10 answers updated

--- Javascript Assignment/Question11/file.js
+++ Javascript Assignment/Question11/file.js
@@ -1,17 +1,30 @@
 // 11. Use the Date object to do the following activities
-//     - What is the year today?
-//     - What is the month today as a number?
-//     - What is the date today?
-//     - What is the day today as a number?
-//     - What is the hours now?
-//     - What is the minutes now?
-//     - Find out the numbers of seconds elapsed from January 1, 1970 to now.
-
 const date = new Date()
+
+// What is the year today?
 console.log(date.getFullYear())
+
+// What is the month today as a number?
 console.log(date.getMonth() + 1)
+
+// What is the date today?
 console.log(date.getDate())
+
+// What is the day today as a number?
 console.log(date.getDay())
-console.log(date.getHours())
-console.log(date.getMinutes())
+
+// What is the hours now?
+console.log(date.getHours()) 
+
+// What is the minutes now?
+console.log(date.getMinutes()) 
+
+// Find out the numbers of seconds elapsed from January 1, 1970 to now.
+console.log(Date.now())
+
+
+
+
+
+
 
