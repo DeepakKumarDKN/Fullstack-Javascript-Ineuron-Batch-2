6552fb447d0db1c7b54a5e81cc8bab85fdc2e7e8
Deepak <deepaknnayak34@gmail.com>
2023-05-31 09:59:38 +0530

    question 12 answers added

--- Javascript Assignment/Question12/file.js
+++ Javascript Assignment/Question12/file.js
@@ -1,7 +1,13 @@
 // 12. Create a human readable time format using the Date time object
 // YYYY-MM-DD HH:mm
-// DD-MM-YYYY HH:mm
-// DD/MM/YYYY HH:mm
+let dt = new Date()
+console.log(`${dt.getFullYear()}-${dt.getMonth() + 1}-${dt.getDate()} ${dt.getHours()}:${dt.getMinutes()}`)
 
-const date = new Date()
-console.log()
+// DD-MM-YYYY HH:mm
+let date = new Date()
+console.log(`${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`)
+
+// DD/MM/YYYY HH:mm
+let d = new Date()
+console.log(d.toLocaleString())
+
