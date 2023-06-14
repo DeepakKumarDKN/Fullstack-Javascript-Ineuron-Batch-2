cc9698f62444b284fae7f44afc33683f70e09417
Deepak <deepaknnayak34@gmail.com>
2023-06-15 00:01:03 +0530

    updated

--- Javascript/8th Jan Prototype - 03-04-23/prototypeTwo.js
+++ Javascript/8th Jan Prototype - 03-04-23/prototypeTwo.js
@@ -1,20 +1,25 @@
 const UserMethods = {
     about : function(){
         return `My Fullname is: ${this.fullname}`
+    },
+    profession : function(){
+        return `${this.job}`
     }
 }
 
-function createUser(firstname,fullname,email){
+function createUser(firstname,fullname,email,job){
     const user = Object.create(UserMethods)
     user.firstname = firstname;
     user.fullname = fullname;
     user.email = email
+    user.job= job
 
     return user
 }
 
-const userOne = createUser('Deepak','Deepak Kumar Nayak','deepakflushymax@gmail.com')
+const userOne = createUser('Deepak','Deepak Kumar Nayak','deepakflushymax@gmail.com', 'Fullstack Developer')
 console.log(userOne.firstname)
 console.log(userOne.fullname)
 console.log(userOne.email)
-console.log(userOne.about())
+console.log(userOne.about()) // getting these methods due to Object.create(UseMethods)
+console.log(userOne.profession()) // getting these methods due to Object.create(UseMethods)
