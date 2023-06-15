4a0a3ea9d04d006ba2cf3b7a33d51044b9dd28d5
Deepak <deepaknnayak34@gmail.com>
2023-06-15 12:17:07 +0530

    prototype and newkeyword example added

--- Javascript/8th Jan Prototype - 03-04-23/prototypeTwo.js
+++ Javascript/8th Jan Prototype - 03-04-23/prototypeTwo.js
@@ -43,3 +43,32 @@ CreateUser.prototype.bio = function(){
 
 const userTwo = CreateUser('rahul','rahul krishna vaidya','rahul@gmail.com', 'Singer')
 console.log(userTwo.bio())
+
+
+
+//New Keyword
+// what new keyword is doing behing the scenes
+
+// creating an empty object it will be done automatically with the help of new keyword we dont have to do it by yourself.
+// this is referring to the empty object.
+// return this 
+
+
+function Data(firstname,lastname,age){
+    this.firstname = firstname
+    this.lastname = lastname
+    this.age = age 
+}
+Data.prototype.is18 = function(){
+    if(`${this.age}` > 18){
+        return `he is an adult`
+    }else{
+        return `he should be above 18`
+    }
+}
+
+
+const data = new Data('Srikant','Dubey',18)
+console.log(data.firstname)
+
+console.log(data.is18())
