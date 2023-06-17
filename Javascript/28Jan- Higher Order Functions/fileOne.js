433608dedaff16ba83c68b4ba2ec67573b053e0e
Deepak <deepaknnayak34@gmail.com>
2023-06-17 19:45:32 +0530

    updated

--- Javascript/28Jan- Higher Order Functions/fileOne.js
+++ Javascript/28Jan- Higher Order Functions/fileOne.js
@@ -185,3 +185,8 @@ function sum(x,y){
 
 let vari = [1,2]
 console.log(sum(...vari))
+
+// Higher order function example 
+
+const aray = [1,2,3,4,5,6,7,8,9,10] 
+aray.forEach(num=>console.log(num*2))
