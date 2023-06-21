7c15855f12fdee49dc9958828dd035153e82995c
Deepak <deepaknnayak34@gmail.com>
2023-06-21 18:22:42 +0530

    dom notes added

---
+++ Javascript/Dom-Part-2/Dom/create.js
@@ -0,0 +1,39 @@
+// function addLanguage(...langName){
+//     for(let i of langName){
+//        const li = document.createElement('li')
+//        li.innerHTML = `${i}`
+//        document.querySelector('.Language').appendChild(li)
+
+//     }
+// }
+
+// const lang = ['Python','Laravel','C++', 'C']
+// addLanguage(...lang)
+
+
+// function addPlayersName(...names){
+//     for(let i of names){
+//         const addli = document.createElement('li')
+//         addli.appendChild(document.createTextNode(`${i}`))
+//         document.querySelector('.players').appendChild(addli)
+
+//     }
+// }
+
+// const pnames = ['Shubman Gill','Ishan Kishan','Virat Kohli','S.K Yadav','Hardkik Pandya','Ravindra Jadega','Yuzi','Sriaj','Mohammed Sami','Jasprit Bumrah']
+// addPlayersName(...pnames)
+
+
+// let title = document.createElement('h1')
+// title.className = "title"
+// // title.innerText = "Javascript Bootcamp 2.0"
+// title.textContent = "Javascript Bootcamp 2.0"
+// title.style.color = "white"
+// document.body.appendChild(title)
+
+
+
+
+
+
+
--- Javascript/Dom-Part-2/Dom/index.js
+++ Javascript/Dom-Part-2/Dom/index.js
@@ -1,8 +1,8 @@
 const day = document.getElementsByClassName('day')
-console.log(day)
+//console.log(day)
 
 const convertintoArray = Array.from(day)
-console.log(convertintoArray)
+//console.log(convertintoArray)
 
 convertintoArray.forEach((elem)=>{
     elem.style.color = "orange"
@@ -14,5 +14,45 @@ const parent = document.querySelector('.parent')
 // console.log(parent.children)
 // console.log(parent.children[0])
 
-console.log(parent.firstElementChild)
-console.log(parent.lastElementChild)
+//console.log(parent.firstElementChild)
+//console.log(parent.lastElementChild)
+
+const dayOne = document.querySelector('.day')
+// console.log(dayOne)
+// console.log(dayOne.parentElement)
+
+//console.log('Nodes', parent.childNodes)
+
+//createElement
+
+const div = document.createElement('div')
+div.className = "main-div"
+div.id = "main-head"
+div.style.backgroundColor = "green"
+div.style.width = "max-content"
+div.style.padding = "5px"
+div.style.fontFamily = "Poppins"
+div.setAttribute('title','generate a title')
+//div.innerText = "Aby Teri Toh "
+const addText = document.createTextNode('hello')
+div.appendChild(addText)
+document.body.appendChild(div)
+//console.log(div)
+
+// remove the element
+
+const element = document.getElementById('main-head')
+//element.remove()
+
+const editText = document.getElementById('main-head')
+//editText.innerText = "Hello edit over"
+
+
+// function addLanguage(langName){
+//     const li = document.createElement('li')
+//     li.innerHTML = `${langName}`
+//     document.body.appendChild(li)
+
+// }
+// addLanguage('Python')
+
