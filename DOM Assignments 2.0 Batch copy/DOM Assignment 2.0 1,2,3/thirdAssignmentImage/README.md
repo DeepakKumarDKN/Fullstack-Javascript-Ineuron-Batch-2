2dfc25ab49ad7f19a13ed4915e905193069432f5
Deepak <deepaknnayak34@gmail.com>
2023-06-24 23:15:03 +0530

    updated

---
+++ DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/thirdAssignmentImage/README.md
@@ -0,0 +1,12 @@
+# Assignment 3 
+Output Image
+![Image](./task1Output.png)
+
+```
+const formOne = document.querySelectorAll('.mainLeftDetails input')
+formOne[0].placeholder = "FSJS 2.0"
+formOne[1].placeholder = 'fsjs@ineuron.ai'
+
+const textArea = document.querySelector('.mainLeftDetails textarea')
+textArea.placeholder = "Hello World"
+```
---
+++ DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/thirdAssignmentImage/thirdassignment.js
@@ -0,0 +1,9 @@
+const formOne = document.querySelectorAll('.mainLeftDetails input')
+formOne[0].placeholder = "FSJS 2.0"
+formOne[1].placeholder = 'fsjs@ineuron.ai'
+
+const textArea = document.querySelector('.mainLeftDetails textarea')
+textArea.placeholder = "Hello World"
+
+const formTwo = document.querySelectorAll('.mainRight form input')
+console.log(formTwo)
