84fc151253d965393714c0d57667789a717252ff
Deepak <deepaknnayak34@gmail.com>
2023-06-27 17:38:42 +0530

    project 9 dom solution added

---
+++ DOM Assignments 2.0 Batch copy/DOM P8/DOM P8/index.js
@@ -0,0 +1,23 @@
+// const getAside = document.querySelector('.new')
+// getAside.style.overflowX = "scroll"
+// getAside.style.outlineColor = "red"
+// getAside.style.outlineStyle = "solid";
+// getAside.style.outlineOffset = "15px";
+// getAside.style.outlineWidth = "4px"
+
+
+// const create_h2 = document.createElement('h2')
+// create_h2.className = 'new-head'
+// create_h2.textContent = 'How exactly does Generative AI impact IT jobs?'
+// getAside.append(create_h2)
+
+
+// const create_pTag =  document.createElement('p')
+// create_pTag.className = 'new-p'
+// create_pTag.textContent = 'According to market intelligence firm Unearth Insight’s study, India’s $245-billion IT services industry employs around 300,000 people in sales and support functions. ​​Out of this, around 50,000 to 60,000 employees’ work will get impacted over the next three to five years.'
+// getAside.append(create_pTag)
+
+
+// const removeBackground = document.querySelector('body')
+// removeBackground.style.backgroundImage = 'none'
+
