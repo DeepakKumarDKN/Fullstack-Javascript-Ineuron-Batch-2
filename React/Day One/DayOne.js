895887c7c1e815cf1c5db6f4e180fe9c4f59c68b
Deepak <deepaknnayak34@gmail.com>
2023-07-02 19:46:33 +0530

    updated

---
+++ React/Day One/DayOne.js
@@ -0,0 +1,15 @@
+// hitesh sir example 
+
+function setState(){
+    let score = 22
+    return score
+}
+
+let myscore = setState()
+myscore;
+myscore = 12 
+console.log(myscore)
+
+let anotherScore = setState()
+console.log(anotherScore)
+
