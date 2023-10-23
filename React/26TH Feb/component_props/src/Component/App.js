db393b00c397fa6f6643ac8b14962156986d4090
Deepak <deepaknnayak34@gmail.com>
2023-10-23 21:32:19 +0530

    props example addd

---
+++ React/26TH Feb/component_props/src/Component/App.js
@@ -0,0 +1,13 @@
+import Cards from "./Cards";
+
+const App =() => {
+  return (
+   <>
+    <Cards name="Deepak"/>
+    <Cards name="Rahul"/>
+    <Cards name="Aly"/>
+   </>
+  );
+}
+
+export default App;
