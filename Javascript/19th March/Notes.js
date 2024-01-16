270f775e5324b22d75d2424ed28b9e31e1ae6443
Deepak <deepaknnayak34@gmail.com>
2024-01-16 10:51:58 +0530

    notes added

--- Javascript/19th March/Notes.js
+++ Javascript/19th March/Notes.js
@@ -4,4 +4,18 @@
  * Request
  * Response
  * URL Query
+ * 
+ * There is a person using a computer and he sends a request an HTTP request using wwww called (world wide web)
+ * and the request goes to the server, so as soon as the request send to the server, server checked what kind of 
+ * request has arrived and based on the request the server will send the response
+ * 
+ * API :- APPLICATION PROGRAMMING INTERFACE
+ * it is a kind of link which carry certain information and to get back the data from the server
+ * suppose we want to search for Inueuron , so google had an API Link which contains some domain name and path 
+ * querystring it went to the server ask to the server for data / or the data informtaion server /
+ * its just like that we want to get data from Ineuron and how we will get that data .. when we will 
+ * search for ineuron google had an api link that contains data about ineuron which got informatin from server 
+ * and then we will get to see a webpage which contain the detail informtaion about ineuron.
+ * 
+ * so api is something which will comunicate with between the client and server
  */
