d2d928e1fb15f30dfe736e06a087a2167f6cb192
Deepak <deepaknnayak34@gmail.com>
2024-01-21 16:43:10 +0530

    updated

---
+++ DSA/stack.js
@@ -0,0 +1,32 @@
+// Stack mein jo element add hota hai woh top ke upar se hi hota hai 
+// and remove bhi top se hi hoga 
+
+
+// operation only on top
+// stack mein ham max value bhi set karte hai jiska matlab hai ki 
+// ham uske andar 10 max elements hi rakh sakhte hein 11 aggar 
+// rakhna hai toh nahi aa paenge.
+
+// jahan pe stack and queue ham use karenge wahan ham element ko fix hi rakhenge
+// stack pe kya kya operation kar sakhte hain 
+// push - new element 
+// pop - remove element 
+// isempty() :- yeak ek function hai jo hame check karne ke liye help hoga
+// ki stack empty hai ya nahi 
+// peak : top pe jo data rakha hua hai usse dikhta hai. 
+// seize : seize ek function hame banana rahega ki kitne data es data structure mein stored hein
+
+
+// stack mein ham bas us data ko dekh paenge jo sabse last mein push kiya gaya ho usko nikale bina 
+// ham uske baad wale element ko nahi dekh sakhte hein.
+
+// mainly push and pop hi use hota hai.
+
+// stack ek linear data-structure hai work on the principle of LIFO . 
+// Linear matlab jismein position matter arta hai ek sequence matter karta hai
+
+// stack ek aisa data structure ha jis aap data rakhte jaoge matlab push karenge 
+// andar kaisa store ho raha hein usse hame koi matlab nahi hai par jab ham data 
+// nikalenge hame bas woi data milega jo ki akhiri mein push kiya gaya hoga 
+
+// 
